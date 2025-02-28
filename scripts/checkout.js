e078e9bf273d32a2b7f69d8e73830bd8a4d1cfb3
Enrique <enriqueshanna00@gmail.com>
2025-02-28 13:42:35 +0800

    create class version of event cart

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,5 +1,5 @@
 import { renderOrderSummary } from './checkout/orderSummary.js';
 import { renderPaymentSummary } from './checkout/paymentSummary.js';
-import '../data/eventcart-oop.js';
+import '../data/eventcart-class.js';
 renderOrderSummary();
 renderPaymentSummary();
